b56be28e653ebd6edfa390a8e717a3d1e5ca5d6f
daydream54 <happidev54524@gmail.com>
2023-05-25 12:45:34 +0900

    some styles changed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Encocare</title>
         @viteReactRefresh 
         @vite(['resources/sass/app.scss', 'resources/js/app.jsx'])
         <!-- As you can see, we will use vite with jsx syntax for React-->
